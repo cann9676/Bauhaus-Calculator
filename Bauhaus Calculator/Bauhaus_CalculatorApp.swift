28a146d8ae379738d227733d934651d76ddf09ea
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-09-12 21:13:02 -0400

    Initial Commit

---
+++ Bauhaus Calculator/Bauhaus_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bauhaus_CalculatorApp.swift
+//  Bauhaus Calculator
+//
+//  Created by Chelsea Hannah on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Bauhaus_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bauhaus Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bauhaus Calculator
+//
+//  Created by Chelsea Hannah on 9/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
